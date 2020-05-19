f168179ebf91af8bb4975def8e507a4edf6c4e6b
Cole Rau <colevrau@gmail.com>
2020-05-18 22:10:49 -0700

    mvp achieved

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -18,4 +18,37 @@ class ReviewsController < ApplicationController
             erb :'reviews/show'
         end 
     end
+
+    get '/reviews/:id/edit' do 
+        if !logged_in?
+            redirect "/login"
+        else 
+            @user = current_user
+            @review = Review.find_by_id(params[:id])
+            erb :'/reviews/edit'
+        end 
+    end
+
+    patch '/reviews/:id' do 
+        if !logged_in?
+            redirect "/login"
+        else 
+            @user = current_user
+            @review = Review.find_by_id(params[:id])
+            @review.text = params[:new_review]
+            @review.save
+            redirect to "/reviews/#{@review.id}"
+        end 
+    end
+
+    delete '/reviews/:id' do 
+        if !logged_in?
+            redirect "/login"
+        else 
+            @user = current_user
+            @review = Review.find_by_id(params[:id])
+            @review.delete
+            redirect to '/users/user_home'
+        end 
+    end
 end
